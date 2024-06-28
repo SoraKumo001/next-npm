0f15a8597023bd6a90c418425d911eef7450fed4
SoraKumo <info@croud.jp>
2024-06-28 09:10:05 +0900

    update-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,31 +18,31 @@ const Page = () => {
   const name = router.query["name"];
   const sortIndex = Number(router.query["sort"] || "0");
   useEffect(() => {
-    // if (name) {
-    //   fetch(
-    //     `https://registry.npmjs.org/-/v1/search?text=maintainer:${name}&size=1000`,
-    //   )
-    //     .then((r) => r.json())
-    //     .then((r: NpmType) => {
-    //       setValue(r);
-    //       r.objects.forEach((npm) => {
-    //         const name = npm.package.name;
-    //         setDownloads((v) => ({ ...v, [name]: Array(3).fill(undefined) }));
-    //         const periods = ["last-year", "last-week", "last-day"] as const;
-    //         periods.forEach((period, index) => {
-    //           fetch(`https://api.npmjs.org/downloads/point/${period}/${name}`)
-    //             .then((r) => r.json())
-    //             .then((r) => {
-    //               setDownloads((v) => {
-    //                 const d: number[] = v[name] ?? [];
-    //                 d[index] = r.downloads;
-    //                 return { ...v, [name]: d };
-    //               });
-    //             });
-    //         });
-    //       });
-    //     });
-    // }
+    if (name) {
+      fetch(
+        `https://registry.npmjs.org/-/v1/search?text=maintainer:${name}&size=1000`,
+      )
+        .then((r) => r.json())
+        .then((r: NpmType) => {
+          setValue(r);
+          r.objects.forEach((npm) => {
+            const name = npm.package.name;
+            setDownloads((v) => ({ ...v, [name]: Array(3).fill(undefined) }));
+            const periods = ["last-year", "last-week", "last-day"] as const;
+            periods.forEach((period, index) => {
+              fetch(`https://api.npmjs.org/downloads/point/${period}/${name}`)
+                .then((r) => r.json())
+                .then((r) => {
+                  setDownloads((v) => {
+                    const d: number[] = v[name] ?? [];
+                    d[index] = r.downloads;
+                    return { ...v, [name]: d };
+                  });
+                });
+            });
+          });
+        });
+    }
   }, [name]);
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
@@ -77,7 +77,7 @@ const Page = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="flex  gap-2 p-1">
+      <form onSubmit={handleSubmit} className="flex gap-2 p-1">
         <input
           name="maintainer"
           className="input input-bordered w-full max-w-xs"
@@ -86,7 +86,7 @@ const Page = () => {
         <button className="btn">設定</button>
       </form>
 
-      <table className="table [&_*]:border-gray-300 [&_td]:border-x [&_td]:py-1 [&_th:hover]:bg-slate-200 [&_th]:border-x">
+      <table className="table [&_*]:border-gray-300 [&_td]:border-x [&_td]:py-1 [&_th:hover]:bg-slate-100 [&_th]:border-x">
         <thead className="">
           <tr className="sticky top-0 cursor-pointer bg-white text-lg font-semibold">
             {["index", "date", "name", "year", "week", "day"].map(
